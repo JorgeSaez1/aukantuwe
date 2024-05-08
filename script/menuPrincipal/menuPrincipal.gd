7697d3212cc465c2f9e57a24c2abd308c21f9311
JorgeSaez1 <jorgesaez952o@gmail.com>
2024-05-08 01:52:37 -0400

    interaccion menu principal

--- script/menuPrincipal/menuPrincipal.gd
+++ script/menuPrincipal/menuPrincipal.gd
@@ -9,3 +9,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_inicio_pressed():
+
+	get_tree().change_scene_to_file("res://scenes/menuEtapas/menuEtapas.tscn")
+
+
+func _on_configuracion_pressed():
+	
+	get_tree().change_scene_to_file("res://scenes/menuPrincipal/configuracion.tscn")
+
+
+func _on_creditos_pressed():
+	
+	get_tree().change_scene_to_file("res://scenes/menuPrincipal/creditos.tscn")
+
+
+func _on_salida_pressed():
+	
+	get_tree().quit()

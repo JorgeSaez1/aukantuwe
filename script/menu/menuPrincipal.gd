356aaefd1471ea62ae826fbba910e8de34ed3c78
JorgeSaez1 <jorgesaez952o@gmail.com>
2024-05-19 19:35:41 -0400

    bug en menu etapas

--- script/menu/menuPrincipal.gd
+++ script/menu/menuPrincipal.gd
@@ -7,23 +7,23 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
 func _on_inicio_pressed():
 
-	get_tree().change_scene_to_file("res://scenes/menuEtapas/menuEtapas.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu etapas/menuEtapas.tscn")
 
 
 func _on_configuracion_pressed():
 	
-	get_tree().change_scene_to_file("res://scenes/menuPrincipal/configuracion.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu principal/configuracion.tscn")
 
 
 func _on_creditos_pressed():
 	
-	get_tree().change_scene_to_file("res://scenes/menuPrincipal/creditos.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu principal/creditos.tscn")
 
 
 func _on_salida_pressed():
